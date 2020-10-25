86756b1b9457fc8d992e691ce241bd5e5c0edf43
Marko <tomicmark3@gmail.com>
2020-10-25 12:52:56 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
+import Values from "values.js";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(
+    new Values("#f15025").all(10)
+  );
+  const [number, setNumber] = useState("");
+  const handleChange = (e) => {
+    setColor(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let num = parseInt(number);
+      let colors = new Values(color).all(num);
+      setList(colors);
+      setError(false);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+
+    setColor("");
+    setNumber("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <section className='color-picker'>
+        <form
+          className='form'
+          onSubmit={handleSubmit}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <label htmlFor='picker'>
+            Color Generator
+          </label>
+          <input
+            type='text'
+            name='picker'
+            id='picker'
+            value={color}
+            onChange={handleChange}
+            className={`${
+              error ? "error" : null
+            }`}
+          ></input>
+
+          <label htmlFor='picker2'>
+            Number of Colors
+          </label>
+          <input
+            type='text'
+            name='picker'
+            id='picker'
+            value={number}
+            onChange={(e) =>
+              setNumber(e.target.value)
+            }
+          ></input>
+          <button type='submit' className='btn'>
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
   );
 }
 
---
+++ src/SingleColor.js
@@ -0,0 +1,43 @@
+import React, {
+  useState,
+  useEffect,
+} from "react";
+import rgbToHex from "./utilis";
+const SingleColor = ({
+  rgb,
+  weight,
+  index,
+  hexColor,
+}) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+
+  const hex = rgbToHex(...rgb);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+  return (
+    <article
+      className={`color ${
+        index >= 10 && "color-light"
+      }`}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hex);
+      }}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hex}</p>
+      {alert && (
+        <p className='alert'>
+          copied to clipboard
+        </p>
+      )}
+    </article>
+  );
+};
+export default SingleColor;
